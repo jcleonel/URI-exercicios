1246b7dd3e3b492f13aca843b87dbc694a94513c
Jean Leonel <jean.leonel.costa@gmail.com>
2020-11-29 17:49:40 -0300

    1002 - Área do Círculo

---
+++ src/exercicios/Uri1002.java
@@ -0,0 +1,26 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1002 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);		
+		
+		double raio, area;
+		double pi = 3.14159;
+		
+		raio = sc.nextDouble();
+		
+		area = pi * (Math.pow(raio,2));
+		
+		System.out.printf("A=%.4f", area);
+		System.out.println();
+		sc.close();
+		
+	}
+
+}
