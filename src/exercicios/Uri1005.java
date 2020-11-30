e2d349928f59965e7879b1e58ee6c65b7bc64a2c
Jean Leonel <jean.leonel.costa@gmail.com>
2020-11-30 16:25:52 -0300

    1005 - Média 1

---
+++ src/exercicios/Uri1005.java
@@ -0,0 +1,26 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Uri1005 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double A, B, MEDIA;
+		A = sc.nextDouble();
+		B = sc.nextDouble();
+		
+		MEDIA = ((A * 3.5) + (B * 7.5)) / 11;
+		
+		System.out.printf("MEDIA = %.5f", MEDIA);
+		System.out.println();
+		
+		sc.close();
+
+	}
+
+}
