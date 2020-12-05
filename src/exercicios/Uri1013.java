637abefa912531c5c73bc60ca3a73c4832279c63
Jean Leonel <jean.leonel.costa@gmail.com>
2020-12-05 18:34:40 -0300

    1013 - O Maior

---
+++ src/exercicios/Uri1013.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Uri1013 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+
+		int maiorAB = (a + b + (Math.abs(a - b))) / 2;
+		int maior = (maiorAB + c + (Math.abs(maiorAB - c))) / 2;
+
+		System.out.println(maior + " eh o maior");
+
+		sc.close();
+
+	}
+}
