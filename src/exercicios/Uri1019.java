5d679a4f43c8f362769806945c5c4c3061b61896
Jean Leonel <jean.leonel.costa@gmail.com>
2020-12-19 19:34:33 -0300

    1019 - Conversão de Tempo

---
+++ src/exercicios/Uri1019.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Uri1019 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int horas = N / 3600;
+		int minutos = (N % 3600) / 60;
+		int segundos = N % 60;
+		
+		System.out.println(horas + ":" + minutos + ":" + segundos);
+		
+		sc.close();
+
+	}
+
+}
